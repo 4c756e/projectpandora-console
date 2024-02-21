426528d564d1b8d48590270271f0cada4f7244ae
Rafael M <4c756e@gmail.com>
2024-02-21 03:40:08 +0000

    start

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace projectpandora_console;
+using System; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Read();
+    }
+}
